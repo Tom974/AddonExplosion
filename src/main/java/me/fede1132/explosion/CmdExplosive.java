c8658f30e3ea0a5a809b9778f71b88c54feaef76
Fede <fendt873@gmail.com>
2020-08-31 22:52:52 +0200

    Help menu Tokens system modified from int system to long system

--- src/main/java/me/fede1132/explosion/CmdExplosive.java
+++ src/main/java/me/fede1132/explosion/CmdExplosive.java
@@ -3,6 +3,7 @@ package me.fede1132.explosion;
 import me.fede1132.f32lib.other.StringUtil;
 import me.fede1132.plasmaprisoncore.addons.cmds.CommandInfo;
 import me.fede1132.plasmaprisoncore.addons.cmds.XCommand;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.List;
 public class CmdExplosive extends XCommand {
     private final Explosion instance = ((Explosion) Explosion.getInstance());
     @Override
-    public void onCommand(Player p, String[] args) {
+    public void onCommand(CommandSender sender, Player p, String[] args) {
         boolean curr = instance.toggleExplosive(p.getUniqueId());
         p.sendMessage(StringUtil.color(instance.config.getString("messages.explosive-toggle-" + curr)));
     }
